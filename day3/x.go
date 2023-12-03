caaced632be87de21925d2d572b292de45f5542c
gdh1c <v.eriksson@gmail.com>
2023-12-03 17:34:35 +0100

    parsing board

--- day3/x.go
+++ day3/x.go
@@ -20,6 +20,26 @@ type board struct {
 	syms []int
 }
 
+func parse(input []string) *board {
+	var vals []rune
+	var syms []int
+
+	for y := 0; y < len(input); y++ {
+		for x := 0; x < len(input[y]); x++ {
+			chr := rune(input[y][x])
+			vals = append(vals, chr)
+			if chr != '.' && !unicode.IsDigit(chr) {
+				syms = append(syms, utils.XYToI(x, y, len(input[y])))
+			}
+		}
+	}
+
+	return &board{
+		vals,
+		syms,
+	}
+}
+
 func silver(input []string) int {
 	return 0
 }
