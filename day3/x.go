4c8e337dade91b31ac4d0ce79062b101a64a63fc
gdh1c <v.eriksson@gmail.com>
2023-12-03 17:18:56 +0100

    adding day 3

---
+++ day3/x.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+var TestInput = []string{
+	"467..114..",
+	"...*......",
+	"..35..633.",
+	"......#...",
+	"617*......",
+	".....+.58.",
+	"..592.....",
+	"......755.",
+	"...$.*....",
+	".664.598..",
+}
+
+func silver(input []string) int {
+	return 0
+}
+func main() {
+	fmt.Printf("TEST SILVER: %d\n", silver(TestInput))
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module aoc2023
 
-go 1.21.3
+go 1.18
